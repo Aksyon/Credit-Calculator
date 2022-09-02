86a6c2286eb7f62f87885bae19bde7e22a3eff92
Aksyon <aksenovaleksander94@gmail.com>
2022-09-02 11:49:44 +0500

    Refreshed classes.

--- credit_calculator_module.py
+++ credit_calculator_module.py
@@ -7,26 +7,44 @@ class Credit():
     interest: float
     downpayment: float
     term: int
+    total_interest: float = 0
 
 
 @dataclass
 class AnnuityPayment(Credit):
 
-    def get_payment_month(self):
+    def get_payment_month(self):    #Total month payment
         return self.amount * (self.interest/100/12 + self.interest/100/12 / ((1 + self.interest/12/100)**self.term*12 - 1))
 
-    def get_interest_amount(self):
+    def get_total_interest(self):
         for month in range(self.term * 12):
-            interest_amount = self.amount * self.interest / 12 / 100
-            self.amount = self.amount - (self.get_payment_month - interest_amount)
-            print(f'В {month} месяц проценты по кредиту составят {interest_amount} рублей.') 
+            self.total_interest += self.amount * self.interest / 12 / 100
+            self.amount = self.amount - (self.get_payment_month - self.amount * self.interest / 12 / 100)
+        return self.total_interest
 
     def total_amount(self):
         return self.get_payment_month * self.term * 12
 
         
-
+@dataclass
 class DifferentialPayment(Credit):
 
-    def get_payment_month(self):
-        
+    def get_payment_month(self):   # Total month payment
+        return self.amount / self.term / 12 + self.amount * self.interest / 100 / 12
+
+    def get_total_interest(self):
+        debt_month = self.amount / self.term / 12
+        total_debt = self.amount
+        for month in range (self.term * 12):
+            self.total_interest += total_debt * self.interest / 100 / 12
+            total_debt -= debt_month
+        return self.total_interest
+
+    def get_total_amount(self):
+        return self.amount + self.get_total_interest()
+
+
+if __name__ == '__main__':
+
+    
+    
