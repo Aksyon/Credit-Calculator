d001eee11d46779bba33f47062e504e4a4799781
Aksyon <aksenovaleksander94@gmail.com>
2022-09-02 12:16:24 +0500

    Create class InfoMessage.

--- credit_calculator_module.py
+++ credit_calculator_module.py
@@ -9,6 +9,13 @@ class Credit():
     term: int
     total_interest: float = 0
 
+    @staticmethod
+    def show_training_info(self):
+        return InfoMessage(
+            month_payment = self.get_payment_month()
+            total_interest = self.get_total_interest()
+            total_payment = self.get_total_amount()
+        )
 
 @dataclass
 class AnnuityPayment(Credit):
@@ -38,11 +45,31 @@ class DifferentialPayment(Credit):
         for month in range (self.term * 12):
             self.total_interest += total_debt * self.interest / 100 / 12
             total_debt -= debt_month
+            #self.term * 12 - 1
         return self.total_interest
 
     def get_total_amount(self):
         return self.amount + self.get_total_interest()
 
+@dataclass
+class InfoMessage():
+    """Class describe info object."""
+    MESSAGE = ('Ежемесячный платеж по кредиту: {month_payment} руб.; '
+               'Общая сумма процентов по кредиту: {total_interest} руб.; '
+               'Общая сумма выплаты: {total_payment} руб.'
+               )
+
+    month_payment: float = None
+    total_interest: float = None
+    total_payment: float = None
+
+    @staticmethod
+    def printing(self):
+        return self.MESSAGE.format(
+            month_payment = '%.2f' % self.month_payment,
+            total_interest = '%.2f' % self.total_interest,
+            total_payment = '%.2f' % self.total_payment
+            )
 
 if __name__ == '__main__':
 
