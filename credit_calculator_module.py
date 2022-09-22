42a6fe170c4675bf6d1528ede663a8dae1c4ffa9
Aksyon <aksenovaleksander94@gmail.com>
2022-09-22 12:22:15 +0700

    Logging added to module.

--- credit_calculator_module.py
+++ credit_calculator_module.py
@@ -1,5 +1,10 @@
 from Levenshtein import distance
 from dataklasses import dataklass
+import logging
+
+logging.basicConfig(filename='credit_logs.log', filemode='w',
+                    format='%(asctime)s %(levelname)s %(message)s')
+
 
 @dataklass
 class Credit():
@@ -8,7 +13,7 @@ class Credit():
     interest: float
     downpayment: float = 0
     term: int = 0
-    total_interest: float = 0
+    total_interest: float
     CONSTANT_1 = 1200
     CONSTANT_2 = 1
     CONSTANT_3 = 12
@@ -110,6 +115,12 @@ def read_data(message):
             result = distance(name, key)
             if result < LEVENSHTEIN_DISTANCE:
                 data_dict[name] = message_dict[key]
+    
+    if len(data_dict)<5:
+        logging.error('One or more parapeters not recognized. '
+                      f'check:\n{message}')
+        raise Exception
+
     return data_dict
 
 def create_credit(data_dict):
@@ -118,41 +129,44 @@ def create_credit(data_dict):
     }
     CALCULATOR_NAMES = ('annuity', 'differential')
     LEVENSHTEIN_DISTANCE = 5
-    
+
     try:
         amount = float(data_dict['amount'])
     except ValueError:
-        print('Cумма кредита должна быть положительным числом'
-              'без кавычек.')
+        logging.error('Amount must be a positive number'
+              'without quotes.', exc_info=True)
         exit()
     if amount < 0:
-        raise Exception ('Сумма кредита не может быть отрицательной.')
+        logging.error ('Amount can not be a negative number.')
+        raise Exception
 
     try:
         interest = float(data_dict['interest'])
     except ValueError:
-        print('Процент по кредиту должен быть'
-              'положительным числом от 0 до 100 без знака %.')
+        logging.error('Interest must be in the range from 0 to 100 without %.',
+                      exc_info=True)
         exit()
     if 0 > interest or interest > 100:
-        raise Exception ('Процент по кредиту не может быть отрицательным'
-                         'и быть больше 100.')
+        logging.error('Interest can not be out of range 0-100.')
+        raise Exception
     
     try:
         downpayment = float(data_dict['downpayment'])
     except ValueError:
-        print ('Первоначальный взнос должен быть в виде числа.')
+        logging.error('Downpayment must be a number.', exc_info=True)
         exit()
     if downpayment < 0:
-        raise Exception ('Первоначальный взнос не может быть меньше 0.')
+        logging.error('Downpayment can not be less then 0.')
+        raise Exception
     
     try:
         term = int(data_dict['term'])
     except ValueError:
-        print('Срок кредитования должен быть целым числом.')
+        logging.error('Term must be an integer.')
         exit()
     if term < 0:
-        raise Exception ('Срок кредитования не может быть отрицательным.')
+        logging.error('Term can not be a negative number.')
+        raise Exception
     
     for name in CALCULATOR_NAMES:  
         if distance(name, data_dict['calculator']) < LEVENSHTEIN_DISTANCE:
@@ -162,8 +176,8 @@ def create_credit(data_dict):
                                                        )
             break
         else:
-            raise Exception('Введите annuity или differential.')
-    
+            logging.error('Input <annuity> or <differential>.')
+            raise Exception
     return credit_object
     
 
@@ -173,7 +187,7 @@ def main(credit):
     print(InfoMessage.printing(info))
 
 if __name__ == '__main__':
-    message = 'amnt: 1000000\nintresy: 5.5\ndownpament: 20000\ntewm: 7\ncalculator: annuityt.'
+    message = 'amnt: 1000000\ninterest: 5.5\ndownpament: -20000\nterm: 7\ncalculator: annuityt.'
     data = read_data(message)
     credit = create_credit(data)
     main(credit)
